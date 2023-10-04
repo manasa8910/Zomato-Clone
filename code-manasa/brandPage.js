8a7673e67561c52e0e312c9c917bf0fc71c3f8f8
Manasa <manasa8910@gmail.com>
2023-10-04 21:01:07 +0530

    Brand Page

---
+++ code-manasa/brandPage.js
@@ -0,0 +1,48 @@
+function display1() {
+  document.getElementById("tab1").style.display = "block";
+  document.getElementById("tab2").style.display = "none";
+  document.getElementById("tab3").style.display = "none";
+  document.getElementById("tab4").style.display = "none";
+  document.getElementById("tab5").style.display = "none";
+  document.getElementById("tab6").style.display = "none";
+}
+function display2() {
+  document.getElementById("tab2").style.display = "block";
+  document.getElementById("tab1").style.display = "none";
+  document.getElementById("tab3").style.display = "none";
+  document.getElementById("tab4").style.display = "none";
+  document.getElementById("tab5").style.display = "none";
+  document.getElementById("tab6").style.display = "none";
+}
+function display3() {
+  document.getElementById("tab3").style.display = "block";
+  document.getElementById("tab1").style.display = "none";
+  document.getElementById("tab2").style.display = "none";
+  document.getElementById("tab4").style.display = "none";
+  document.getElementById("tab5").style.display = "none";
+  document.getElementById("tab6").style.display = "none";
+}
+function display4() {
+  document.getElementById("tab4").style.display = "block";
+  document.getElementById("tab1").style.display = "none";
+  document.getElementById("tab3").style.display = "none";
+  document.getElementById("tab2").style.display = "none";
+  document.getElementById("tab5").style.display = "none";
+  document.getElementById("tab6").style.display = "none";
+}
+function display5() {
+  document.getElementById("tab5").style.display = "block";
+  document.getElementById("tab1").style.display = "none";
+  document.getElementById("tab3").style.display = "none";
+  document.getElementById("tab4").style.display = "none";
+  document.getElementById("tab2").style.display = "none";
+  document.getElementById("tab6").style.display = "none";
+}
+function display6() {
+  document.getElementById("tab6").style.display = "block";
+  document.getElementById("tab1").style.display = "none";
+  document.getElementById("tab3").style.display = "none";
+  document.getElementById("tab4").style.display = "none";
+  document.getElementById("tab5").style.display = "none";
+  document.getElementById("tab2").style.display = "none";
+}
